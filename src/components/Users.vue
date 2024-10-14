c5439df2955a744a08d18a2d59cceace978b3ef6
medaminebenchouikha <med.amine.ben.chouikha@gmail.com>
2024-10-14 11:03:07 +0200

    create stack

---
+++ src/components/Users.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            title : 'Liste des utilisateurs'
+        }
+    }
+}
+</script>
