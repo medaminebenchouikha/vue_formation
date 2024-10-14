0b5347d7f50939037e81112864c6e271bc78edb8
medaminebenchouikha <med.amine.ben.chouikha@gmail.com>
2024-10-14 11:53:55 +0200

    communication

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,29 @@
 <template>
     <h1>Navbar</h1>
+    <Search :name="myName" @on-search="listenSearch"/>
+    <!-- <Search :name="myName" @on-search="listenSearch($event)"/> -->
+    <button @click="changeName">Changer le nom</button>
 </template>
+
+<script lang="ts">
+import Search from './Search.vue';
+
+export default {
+    data() {
+        return {
+            myName : 'Med',
+        }
+    },
+    methods: {
+        listenSearch(username: string) {
+            console.log(username);   
+        },
+        changeName() {
+            this.myName = 'Djin';
+        }
+    },
+    components: {
+        Search
+    }
+}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <input type="text" v-model="username" >
+        <button @click="onSearch" v-if="username != ''">Rechercher</button>
+    </div>
+    <!-- <div v-else-if="username == 'Med'"> Test</div>
+    <div v-else>Plus de bouton</div> -->
+    <ul>
+        <!-- <li v-for="name in firstNames" :key="name">
+            {{  name  }}
+        </li> -->
+        <!-- <li v-for="(val, key) in myObj" :key="name">
+            {{  key }} - {{  val  }}
+        </li> -->
+        <li v-for="(name, index) in firstNames" :key="name">
+            {{  index }} - {{  name  }}
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+
+export default {
+    // props: ['name'],
+    props: {
+        name: {
+            type: String as PropType<string>,
+            required: true
+        }
+    },
+    emits: ['on-search'],
+    data() {
+        return {
+            username: this.name,
+            firstNames: ['ana', 'Djin', 'Naroto', 'Luffy']
+        }
+    },
+    methods: {
+        onSearch() {
+            this.$emit('on-search', this.username)
+        }
+    },
+    watch: {
+        name(newVal: string) {
+            this.username = newVal;
+        }
+    }
+}
+</script>
