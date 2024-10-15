154d3e7aec6e7d4f1dfbaa76860efcab4780986d
medaminebenchouikha <med.amine.ben.chouikha@gmail.com>
2024-10-15 11:34:35 +0200

    canvas

---
+++ src/components/Draw.vue
@@ -0,0 +1,24 @@
+<template>
+    <canvas :ref="(el) => elCanvas.push(el)"></canvas>
+    <canvas :ref="(el) => elCanvas.push(el)"></canvas>
+    <!-- <canvas ref="elCanvas"></canvas> -->
+    <button @click="drawRectangle">Dessiner rectangle</button>
+</template>
+
+<script setup lang="ts">
+import { reactive, useTemplateRef } from 'vue';
+
+const elCanvas = reactive<any[]>([]);
+
+//const elCanvas = useTemplateRef('elCanvas');
+
+const drawRectangle = () => {
+    for (let el of elCanvas) {
+            const context = el.getContext('2d');
+            if (!context) return;
+            context.fillStyle = 'blue'
+            context.fillRect(0, 0, 100, 100);
+        }
+    }
+    
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,21 @@
 <template>
-    <h1>{{ title }}</h1>
+    <h1 ref="elTitle">{{ title }}</h1>
+    <Draw></Draw>
     <Search :name="myName" @on-search="listenSearch"/>
     <!-- <Search :name="myName" @on-search="listenSearch($event)"/> -->
     <button @click="changeName()">Changer le nom</button>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, useTemplateRef } from 'vue';
 import Search from './Search.vue';
+import Draw from './Draw.vue';
 
 let title = ref('My App');
 const myName = ref('Med');
+const h1 = useTemplateRef('elTitle');
+
+console.log(h1.value);
 
 const changeName = () => {
     myName.value = 'Nouveau titre';
